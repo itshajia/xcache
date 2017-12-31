617c99f0dc4d3eda8856701423e4dec25ad94bf8
Rosco Pecoltran <rosco_pecoltran@msn.com>
2017-12-31 19:03:30 +0100

    initial commit

---
+++ backend/remote/fixity/init.go
@@ -0,0 +1,18 @@
+package fixtycache
+
+/*
+import (
+	"github.com/sniperkit/config"
+	"github.com/sniperkit/logger"
+)
+
+var (
+	logx logger.Logger
+)
+
+func init() {
+	config.AfterInit(func() {
+		logx = logger.New().WithField("pkg", "fixtycache")
+	})
+}
+*/
